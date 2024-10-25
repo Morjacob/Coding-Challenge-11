d8f3a10a8fc89fc92220f5113ccb29f7002ac187
Morgan Jacob <jacobmmori@gmail.com>
2024-10-25 13:03:31 -0400

    fixing minor errors for total price function

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const orderSummary = document.getElementById('order-summary');
 // Calculate price in real time when product or quantity changes
 function updateTotalPrice() {
     const productPrice = parseFloat(productSelector.value);
-    const quantity = parseInt(quantityInput.value);
+    const quantity = parseInt(quantityInput.value) || 0 // intialize with empty input;
     const totalPrice = productPrice * quantity;
     
     totalPriceElement.textContent = totalPrice.toFixed(2);
@@ -24,4 +24,10 @@ placeOrderButton.addEventListener('click', function() {
     const totalPrice = totalPriceElement.textContent;
     
     orderSummary.textContent = `Placed ordered for ${quantity} ${selectedProduct}. Total price: $${totalPrice}`;
+
+    if (quantity > 0) {
+        orderSummary.textContent = `Placed order for ${quantity} ${selectedProduct}. Total price: $${totalPrice}`;
+    } else {
+        orderSummary.textContent = `Please enter a quantity.`;
+    }
 });
